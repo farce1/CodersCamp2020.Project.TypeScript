77076845636312dfa26d20d63cdfd0fcb69c770b
Olaf1303 <olaf.koziara@gmail.com>
2021-01-27 20:52:02 +0100

    format code

--- src/components/Title/Title.ts
+++ src/components/Title/Title.ts
@@ -1,20 +1,18 @@
 const Title = (label: string, id: string): HTMLDivElement => {
-  const AnimatedTitle = document.createElement('div');
-  AnimatedTitle.id = id;
-  AnimatedTitle.setAttribute('data-testid', id);
-  label.split('').map((letter:string,index:number)=>{ 
-    const span = document.createElement('span');
-    span.textContent=letter;
-    span.style.animationDelay=`${100*(label.length-index)}ms`;
-    span.style.animationName=`${id}Animation`
-    AnimatedTitle.appendChild(span);
-    AnimatedTitle.addEventListener('click',()=>{
-      window.location.href='/'
+  const AnimatedTitle = document.createElement('div')
+  AnimatedTitle.id = id
+  AnimatedTitle.setAttribute('data-testid', id)
+  label.split('').map((letter: string, index: number) => {
+    const span = document.createElement('span')
+    span.textContent = letter
+    span.style.animationDelay = `${100 * (label.length - index)}ms`
+    span.style.animationName = `${id}Animation`
+    AnimatedTitle.appendChild(span)
+    AnimatedTitle.addEventListener('click', () => {
+      window.location.href = '/'
     })
-  });
-  AnimatedTitle.className = 'title';
-  return AnimatedTitle;
+  })
+  AnimatedTitle.className = 'title'
+  return AnimatedTitle
 }
 export default Title
-
-

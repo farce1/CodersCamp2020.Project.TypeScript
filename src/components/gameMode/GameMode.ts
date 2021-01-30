beddf83254c89eacabaddd186995576912f8f414
Natalia <natalia132@wp.pl>
2021-01-30 22:51:35 +0100

    classes and overlay layer added to component, scss file with component styles added and imported to overal styles

--- src/components/gameMode/GameMode.ts
+++ src/components/gameMode/GameMode.ts
@@ -7,15 +7,20 @@ export const gameMode = (
   className?: string,
   parentId?: string
 ): HTMLDivElement => {
-  const component: HTMLDivElement = document.createElement('div')!;
-  component.classList.add('mode-tile');
+  const component: HTMLDivElement = document.createElement('div');
+  component.classList.add('game-mode_container');
 
+  //creates overlay appearing on hover
+  const overlay: HTMLDivElement = document.createElement('div');
+  overlay.classList.add('game-mode_overlay');
+  component.appendChild(overlay);
+
+  //setting parametres
   typeof label === 'string' ? handleStringScenario(label, component) : component.appendChild(label);
 
   if (className) {
     component.classList.add(`${className}`);
   }
-  component.innerHTML = `<div class = 'hide'>test</div>`;
   const parentEl = document.getElementById(`${parentId}`)!;
 
   component.addEventListener('click', onClick);

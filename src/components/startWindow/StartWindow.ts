5b6c49d68a05f05a9385bf2b9d84f8a7a1b0081c
miron54 <miron54@op.pl>
2021-01-30 15:13:10 +0100

    Continue

--- src/components/startWindow/StartWindow.ts
+++ src/components/startWindow/StartWindow.ts
@@ -1,1 +1,16 @@
-import { render } from "../../utils/domHandlers";
+import { render } from '../../utils/domHandlers'
+
+export const StartWindow = (
+  label_left: string,
+  label_right: string,
+  parentId?: string,
+  button?: string
+): HTMLElement => {
+  const window = document.createElement('div')
+  const title = document.createElement('div')
+  window.setAttribute('id', 'start-window')
+  window.classList.add('start-window')
+  window.innerText = label_left + label_right
+  const element = parentId && document.getElementById(parentId)
+  return element ? render(window, element) : window
+}

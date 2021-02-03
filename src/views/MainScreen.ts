c0372671a9ecb36d9df70dcdb96caad79fa516b3
olafk <olaf.koziara@gmail.com>
2021-02-03 14:20:28 +0100

    create folder for MainScreen view

--- src/views/MainScreen.ts
+++
@@ -1,6 +0,0 @@
-export const MainScreen = (): HTMLDivElement => {
-  const mainScreen = document.createElement('div')
-  mainScreen.innerText = 'test test test'
-
-  return mainScreen
-}
---
+++ src/views/MainScreen/MainScreen.ts
@@ -0,0 +1,5 @@
+export const MainScreen = (): HTMLDivElement => {
+  const mainScreen = document.createElement('div');
+
+  return mainScreen;
+}

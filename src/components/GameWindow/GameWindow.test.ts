0570a685efe632be0b897d596b5087f71a1fed82
Natalia Dębska <natalia.debska98@gmail.com>
2021-02-08 21:23:43 +0100

    fix gameWindow function

--- src/components/GameWindow/GameWindow.test.ts
+++ src/components/GameWindow/GameWindow.test.ts
@@ -1,19 +1,35 @@
 import gameWindow from './GameWindow';
 
 
-describe('invoke gameWindow', () => {
+describe('Test gameWindow', () => {
+    beforeEach(() => {
+        document.body.innerHTML = '<div id="geo-app"></div>';
+      });
     it('should show europe.svg', () => {
         gameWindow({continent: 'Europe', gamesMode: 'nic'});
-        // console.log('to bedzie div');
-        const appComponent: HTMLElement = document.getElementById('geo-app')!
-        console.log(appComponent);
-        // const element: HTMLElement | null = document.querySelector('.continent');
-        // console.log(element)
-        // console.log(element?.innerText)
-        // expect(element).not.toBeNull;
-        // expect(element?.innerText).not.toBeNull;
+        const element: HTMLElement = document.getElementById('geo-app')!;
+        expect(element.children).not.toBeNull();
+        expect(element.children.length).toEqual(1);
+        expect(element.firstElementChild?.children[0].children.length).toEqual(48);
+        expect(element.firstElementChild?.innerHTML.includes('path')).toBeTruthy();
     });
 
+    it('should show africa.svg', () => {
+        gameWindow({continent: 'Africa', gamesMode: 'nic'});
+        const element: HTMLElement = document.getElementById('geo-app')!;
+        expect(element.children).not.toBeNull();
+        expect(element.children.length).toEqual(1);
+        expect(element.firstElementChild?.children[0].children.length).toEqual(53);
+        expect(element.firstElementChild?.innerHTML.includes('path')).toBeTruthy();
+    });
 
+    it('should show north-america.svg', () => {
+        gameWindow({continent: 'North America', gamesMode: 'nic'});
+        const element: HTMLElement = document.getElementById('geo-app')!;
+        expect(element.children).not.toBeNull();
+        expect(element.children.length).toEqual(1);
+        expect(element.firstElementChild?.children[0].children.length).toEqual(73);
+        expect(element.firstElementChild?.innerHTML.includes('path')).toBeTruthy();
+    });
 }
 )

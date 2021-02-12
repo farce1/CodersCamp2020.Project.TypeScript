e6e4dcda0e2cd71e2b7a27e18809a419cc5fb3e0
Natalia <59486546+talcia@users.noreply.github.com>
2021-02-12 15:16:57 +0100

    add tests to gamesMode div

--- src/components/GameWindow/GameWindow.test.ts
+++ src/components/GameWindow/GameWindow.test.ts
@@ -1,35 +1,37 @@
 import gameWindow from './GameWindow';
 
-
 describe('Test gameWindow', () => {
-    beforeEach(() => {
-        document.body.innerHTML = '<div id="geo-app"></div>';
-      });
-    it('should show europe.svg', () => {
-        gameWindow({continent: 'Europe', gamesMode: 'nic'});
-        const element: HTMLElement = document.getElementById('geo-app')!;
-        expect(element.children).not.toBeNull();
-        expect(element.children.length).toEqual(1);
-        expect(element.firstElementChild?.children[0].children.length).toEqual(48);
-        expect(element.firstElementChild?.innerHTML.includes('path')).toBeTruthy();
-    });
+  beforeEach(() => {
+    document.body.innerHTML = '<div id="geo-app"></div>';
+  });
+  it('should show europe.svg', () => {
+    gameWindow({ continent: 'Europe', gamesMode: 'choseOnMap' });
+    const element: HTMLElement = document.getElementById('geo-app')!;
+    expect(element.children).not.toBeNull();
+    expect(element.children.length).toEqual(2);
+    expect((element.children[0].innerHTML = '<div id="games-mode"><p>flagQuiz</p></div>'));
+    expect(element.lastElementChild?.children[0].children.length).toEqual(48);
+    expect(element.lastElementChild?.innerHTML.includes('path')).toBeTruthy();
+  });
 
-    it('should show africa.svg', () => {
-        gameWindow({continent: 'Africa', gamesMode: 'nic'});
-        const element: HTMLElement = document.getElementById('geo-app')!;
-        expect(element.children).not.toBeNull();
-        expect(element.children.length).toEqual(1);
-        expect(element.firstElementChild?.children[0].children.length).toEqual(53);
-        expect(element.firstElementChild?.innerHTML.includes('path')).toBeTruthy();
-    });
+  it('should show africa.svg', () => {
+    gameWindow({ continent: 'Africa', gamesMode: 'flagQuiz' });
+    const element: HTMLElement = document.getElementById('geo-app')!;
+    expect(element.children).not.toBeNull();
+    expect(element.children.length).toEqual(2);
+    expect((element.children[0].innerHTML = '<div id="games-mode"><p>flagQuiz</p></div>'));
+    expect(element.lastElementChild?.children[0].children.length).toEqual(53);
+    expect(element.lastElementChild?.innerHTML.includes('path')).toBeTruthy();
+  });
 
-    it('should show north-america.svg', () => {
-        gameWindow({continent: 'North America', gamesMode: 'nic'});
-        const element: HTMLElement = document.getElementById('geo-app')!;
-        expect(element.children).not.toBeNull();
-        expect(element.children.length).toEqual(1);
-        expect(element.firstElementChild?.children[0].children.length).toEqual(73);
-        expect(element.firstElementChild?.innerHTML.includes('path')).toBeTruthy();
-    });
-}
-)
+  it('should show north-america.svg', () => {
+    gameWindow({ continent: 'North America', gamesMode: 'true/false' });
+    const element: HTMLElement = document.getElementById('geo-app')!;
+    expect(element.children).not.toBeNull();
+    expect(element.children.length).toEqual(2);
+    expect((element.children[0].innerHTML = '<div id="games-mode"><p>true/false</p></div>'));
+    expect(element.children[1].innerHTML);
+    expect(element.lastElementChild?.children[0].children.length).toEqual(73);
+    expect(element.lastElementChild?.innerHTML.includes('path')).toBeTruthy();
+  });
+});

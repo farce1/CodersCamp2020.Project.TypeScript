921d31e7b7b8b9fb916d965ba4a986f3004816e2
Natalia <59486546+talcia@users.noreply.github.com>
2021-02-12 12:52:49 +0100

    add object

---
+++ src/utils/gamesRules.ts
@@ -0,0 +1,6 @@
+export const gameRules = {
+  choseOnMap: `Sprawdź jak dobrze znasz mapę polityczną danego kontynentu. Wskaż podany kraj na mapie poprzez kliknięcie jego obszaru. 
+	Postaraj się zdobyć jak najmniej punktów karnych, które dodawane są gdy zaznaczony zostanie niewłaściwy kraj. Powodzenia!`,
+  flagsQuiz: `Sprawdź jak dobrze znasz się na flagach. Zaznacz flagę, która reprezentuje dany kraj. Postaraj się zdobyć jak najmniej
+	 punktów karnych, które dodawane są gdy zaznaczony zostanie niewłaściwa flaga. Powodzenia!`,
+};

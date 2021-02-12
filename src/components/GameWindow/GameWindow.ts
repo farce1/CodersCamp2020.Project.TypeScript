96a03a617595217a6d301a6968d2c9a3fe49e2eb
Natalia <59486546+talcia@users.noreply.github.com>
2021-02-12 15:16:38 +0100

    add gamesMode div

--- src/components/GameWindow/GameWindow.ts
+++ src/components/GameWindow/GameWindow.ts
@@ -1,25 +1,31 @@
 import * as fs from 'fs';
 import { addElementToParent } from '../../utils/addElementToParent/addElementToParent';
 
-const gameWindow = (gamesOption: {continent: string, gamesMode: string}) => {
-    const parentDiv: HTMLElement = document.createElement('div');
-    parentDiv.className = 'continent';
-    switch (gamesOption.continent) {
-        case 'Europe':
-            parentDiv.innerHTML = fs.readFileSync('src/assets/svg/europe.svg', 'utf-8');
-            break;
-        case 'Africa':
-            parentDiv.innerHTML = fs.readFileSync('src/assets/svg/africa.svg', 'utf-8');
-            break;
-        case 'North America':
-            parentDiv.innerHTML = fs.readFileSync('src/assets/svg/north-america.svg', 'utf-8');
-            break
-        default:
-            parentDiv.innerHTML = '';
-            break;
-    }
-    addElementToParent('geo-app', parentDiv)
-}
-
+const gameWindow = (gamesOption: { continent: string; gamesMode: string }) => {
+  const parentDiv: HTMLElement = document.createElement('div');
+  parentDiv.className = 'continent';
+  switch (gamesOption.continent) {
+    case 'Europe':
+      parentDiv.innerHTML = fs.readFileSync('src/assets/svg/europe.svg', 'utf-8');
+      break;
+    case 'Africa':
+      parentDiv.innerHTML = fs.readFileSync('src/assets/svg/africa.svg', 'utf-8');
+      break;
+    case 'North America':
+      parentDiv.innerHTML = fs.readFileSync('src/assets/svg/north-america.svg', 'utf-8');
+      break;
+    default:
+      parentDiv.innerHTML = '';
+      break;
+  }
+  const gamesModeDiv: HTMLElement = document.createElement('div');
+  const gamesModep: HTMLElement = document.createElement('p');
+  gamesModeDiv.id = 'games-mode';
+  gamesModep.className = 'games-mode';
+  gamesModep.innerText = gamesOption.gamesMode;
+  addElementToParent('games-mode', gamesModep);
+  addElementToParent('geo-app', gamesModeDiv);
+  addElementToParent('geo-app', parentDiv);
+};
 
 export default gameWindow;

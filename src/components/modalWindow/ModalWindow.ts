26858d7526f094355e77d07bc3075c0ff2577123
Izabela <noris2106@gmail.com>
2021-02-14 20:00:50 +0100

    Okno_modalne_#9

--- src/components/modalWindow/ModalWindow.ts
+++ src/components/modalWindow/ModalWindow.ts
@@ -24,31 +24,3 @@ export const ModalWindow = (componentToDisplay: component, close: (e:Event) => v
 
 
 
-
-/*const TestElement = document.createElement('div');
-TestElement.classList.add('testElement');
-const xbtn=document.createElement('button');
-xbtn.classList.add("xbtn");
-TestElement.appendChild(xbtn);
-
-const tElement=`<div class="testElement"><button class="xbtn"></button></div>`;
-
-
-
-const testFun = (e:Event) => {
-    const btn=document.querySelector('.xbtn');
-    const mv=document.querySelector('.modal-window-visible');
-    const body=document.querySelector('body');
-    if(e.target===btn){
-    mv.classList.remove('modal-window-visible');
-  body.removeChild(mv);
-    }
-
-}
-
-let mw=ModalWindow(tElement, testFun);
-let body=document.querySelector('body');
-if(body){
-body.appendChild(mw);
-}*/
-

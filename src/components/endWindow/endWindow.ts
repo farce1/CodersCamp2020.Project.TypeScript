1fc939bb3d3f51672eb9488a0d105bd81a5961f0
Natalia <59486546+talcia@users.noreply.github.com>
2021-02-16 15:24:02 +0100

    fix error label

--- src/components/endWindow/endWindow.ts
+++ src/components/endWindow/endWindow.ts
@@ -3,16 +3,24 @@ import { addElementToParent } from '../../utils/addElementToParent/addElementToP
 import { render } from '../../utils/domHandlers';
 import { MainScreen } from '../../views/MainScreen';
 import { removeElementFromParent } from '../../utils/removeElementFromParent/removeElementFromParent';
+import { openStartWindow } from '../../utils/constsTagElementsAndWindows';
 
 export const endWindow = (goodAnserw: String) => {
   const appComponent: HTMLElement = document.getElementById('geo-app')!;
   const wrongAnswersCount = localStorage.getItem('numberOfWrongAnswers');
   const parentDiv = document.createElement('div');
   parentDiv.id = 'end-window';
+  let error = '';
+  if (wrongAnswersCount === '1') {
+    error = 'błąd';
+  } else if (wrongAnswersCount === '2' || wrongAnswersCount === '3' || wrongAnswersCount === '4') {
+    error = 'błędy';
+  } else {
+    error = 'błędów';
+  }
+
   parentDiv.innerHTML = `<h1>Koniec gry</h1>
-						<p>Na <span>${goodAnserw}</span> rundy popełniłeś <span>${wrongAnswersCount}</span> ${
-    wrongAnswersCount === '1' ? 'błąd' : 'błędów'
-  }</p>
+						<p>Na <span>${goodAnserw}</span> rundy popełniłeś <span>${wrongAnswersCount}</span> ${error}</p>
 							`;
   localStorage.setItem('numberOfWrongAnswers', '0');
 
@@ -36,6 +44,7 @@ export const endWindow = (goodAnserw: String) => {
       () => {
         localStorage.setItem('numberOfWrongAnswers', '0');
         //   render(StartScreen(), appComponent);
+        openStartWindow();
         removeElementFromParent('geo-app', 'end-window');
       },
       'back-to-menu',

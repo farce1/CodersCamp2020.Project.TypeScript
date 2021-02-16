2518a75a06ae3a9ff4f9560e2312f52a6dd5572b
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2021-02-16 17:39:49 +0100

    Fix literalls

--- src/components/endWindow/endWindow.ts
+++ src/components/endWindow/endWindow.ts
@@ -1,5 +1,4 @@
 import { Button } from '../button/Button';
-import { addElementToParent } from '../../utils/addElementToParent/addElementToParent';
 import { render } from '../../utils/domHandlers';
 import { MainScreen } from '../../views/MainScreen';
 import { removeElementFromParent } from '../../utils/removeElementFromParent/removeElementFromParent';
@@ -10,17 +9,9 @@ export const endWindow = (goodAnserw: String) => {
   const wrongAnswersCount = localStorage.getItem('numberOfWrongAnswers');
   const parentDiv = document.createElement('div');
   parentDiv.id = 'end-window';
-  let error = '';
-  if (wrongAnswersCount === '1') {
-    error = 'błąd';
-  } else if (wrongAnswersCount === '2' || wrongAnswersCount === '3' || wrongAnswersCount === '4') {
-    error = 'błędy';
-  } else {
-    error = 'błędów';
-  }
 
   parentDiv.innerHTML = `<h1>Koniec gry</h1>
-						<p>Na <span>${goodAnserw}</span> rundy popełniłeś <span>${wrongAnswersCount}</span> ${error}</p>
+						<p>Rozegrane rundy: <span>${goodAnserw}</span>, Niepoprawne: <span>${wrongAnswersCount}</span></p>
 							`;
   localStorage.setItem('numberOfWrongAnswers', '0');
 
@@ -43,7 +34,6 @@ export const endWindow = (goodAnserw: String) => {
       'Wróć do menu',
       () => {
         localStorage.setItem('numberOfWrongAnswers', '0');
-        //   render(StartScreen(), appComponent);
         openStartWindow();
         removeElementFromParent('geo-app', 'end-window');
       },
